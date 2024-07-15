96d7c24fa3427d1cba5a59bbbb5e69bee2adc584
jjolly04 <judejolly2004@gmail.com>
2024-07-15 16:53:38 -0500

    Make main files, create basic 4 math functions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # calculator
+Culminating the intersection of HTML, CSS, and Javascript in making this online calculator
---
+++ script.js
@@ -0,0 +1,7 @@
+//the four fundamental math functions
+let add = (x, y) => x + y;
+let subtract = (x, y) => x - y;
+let multiply = (x, y) => x * y;
+let divide = (x, y) => x / y;
+
+
