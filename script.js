a86b89df94ea8043007cdc65002deda7f5ff66f6
jjolly04 <judejolly2004@gmail.com>
2024-07-15 17:56:55 -0500

    Add plain calculator gui with display and buttons

--- script.js
+++ script.js
@@ -4,4 +4,27 @@ let subtract = (x, y) => x - y;
 let multiply = (x, y) => x * y;
 let divide = (x, y) => x / y;
 
+let firstNum = 10234;
+let operator = '*';
+let secondNum = -42114;
 
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+
+    case "-":
+      return subtract(num1, num2);
+
+    case "*":
+      return multiply(num1, num2);
+
+    case "/":
+      return divide(num1, num2);
+
+    default:
+      return "Invalid input in operate function";
+  }
+}
+
+console.log(operate(operator, firstNum, secondNum));
