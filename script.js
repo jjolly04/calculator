1385cab3c8fd85dec3aef9dbc161b1a36fd87bf8
jjolly04 <judejolly2004@gmail.com>
2024-07-15 22:54:43 -0500

    Add logic for multiple operator input
    Added the logic to calculate the result of the first pair of numbers if
    more than one operator is input at a time. The result is then shown in
    the display with the second, recently input operator following.

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ let subtract = (x, y) => x - y;
 let multiply = (x, y) => x * y;
 let divide = (x, y) => x / y;
 
-let firstNum = 10234;
-let operator = "*";
-let secondNum = -42114;
+function findCountOfOperator(str) {
+  //used to find num of instances of any operator in display
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (operators.includes(str.charAt(i))) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+const operators = "+-x/"; //list of operators used for comparisons later on
+const numbers = "1234567890";
 
 function operate(operator, num1, num2) {
   switch (operator) {
@@ -16,7 +27,7 @@ function operate(operator, num1, num2) {
     case "-":
       return subtract(num1, num2);
 
-    case "*":
+    case "x":
       return multiply(num1, num2);
 
     case "/":
@@ -27,28 +38,92 @@ function operate(operator, num1, num2) {
   }
 }
 
-console.log(operate(operator, firstNum, secondNum));
-
 //logic to update the display when a button is pressed
 let displayText = "";
-
 const display = document.querySelector(".display");
-
 const buttons = document.querySelectorAll(".on-screen"); //select only the buttons whose actual text should appear on screen (no = or clear)
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    displayText += button.textContent + " ";
+    if (numbers.includes(button.textContent)) {
+      displayText += button.textContent;
+    } else {
+      displayText += " " + button.textContent + " ";
+      let countOperators = findCountOfOperator(displayText);
+
+      //if there are more than 1 operations than calculate result of first two numbers first and then display that with the new operator
+      if (countOperators > 1) {
+        let endOperator = displayText.split(" ").at(-2);
+
+        equalsButton.click();
+
+        displayText += " " + endOperator + " ";
+      }
+    }
+
     display.textContent = displayText;
   });
 });
 
+
 const clearButton = document.querySelector(".clear");
-const equalsButton = document.querySelector("equals");
 
 clearButton.addEventListener("click", () => {
   displayText = "";
   display.textContent = "";
+  firstNum = "";
+  operator = "";
+  secondNum = "";
 });
 
+//add logic for equals button to make result appear on screen
+const equalsButton = document.querySelector(".equals");
 
+let firstNum = "";
+let operator = "";
+let secondNum = "";
+
+equalsButton.addEventListener("click", () => {
+  let splitDisplayText = displayText.split(" ");
+
+  if (operators.includes(splitDisplayText.at(-1))) {
+    //splice current calculator text into array to input into operate
+    splitDisplayText.splice(-2, 2); //chop off end to just get operands and operator
+    console.log(splitDisplayText);
+  }
+
+  let operatorIndex = 0;
+
+  //get everything before the operator in firstNum and everything after in secondNum
+  for (
+    operatorIndex;
+    operatorIndex < splitDisplayText.length;
+    operatorIndex++
+  ) {
+    if (operators.includes(splitDisplayText[operatorIndex])) {
+      break;
+    }
+    firstNum += splitDisplayText[operatorIndex];
+  }
+
+  operator = splitDisplayText[operatorIndex];
+
+  operatorIndex++;
+
+  for (
+    operatorIndex;
+    operatorIndex < splitDisplayText.length;
+    operatorIndex++
+  ) {
+    secondNum += splitDisplayText[operatorIndex];
+  }
+
+  let result = operate(operator, +firstNum, +secondNum);
+
+  displayText = result;
+  display.textContent = result;
+
+  //reset for future calculations
+  firstNum = "";
+  secondNum = "";
+});
